41406dfc67ff6bc0a342a7c7323617cd8f883eb3
Scott Ian <sim2487@gmail.com>
2021-05-16 19:27:20 -0700

    Refactor day01 and add day02 files.

--- day01/index.js
+++ day01/index.js
@@ -6,13 +6,21 @@ const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
 const inputArr = input.split('\n').map(Number);
 
 //loop through all the items and for each loop through all the other items to check if they add up to 2020, then multiply those two items for answer
-const product = (array => {
+const answerOne = (array => {
 	for (let i = 0; i < inputArr.length; i++) {
 		for (let j = 1; j < inputArr.length; j++) {
 			if (inputArr[i] + inputArr[j] == 2020) {
 				// console.log(inputArr[i], inputArr[j]); // 1137 and 883
 				console.log('Part one: ', inputArr[i] * inputArr[j]); // 1003971
+				return;
 			}
+		}
+	}
+})();
+
+const answerTwo = (array => {
+	for (let i = 0; i < inputArr.length; i++) {
+		for (let j = 1; j < inputArr.length; j++) {
 			for (let k = 2; k < inputArr.length; k++) {
 				// for some reason 0 is included in the array when ran, which ruins the answer, so checking that
 				if (inputArr[i] !== 0 && inputArr[j] !== 0 && inputArr[k] !== 0) {
---
+++ day02/index.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = require('path');
+const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
+console.log(input);
