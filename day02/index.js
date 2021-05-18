a47ef47cfe4548e8c7edcc03575edc4bf7573825
Scott Ian <sim2487@gmail.com>
2021-05-18 14:24:09 -0700

    Day02 part two solution.

--- day02/index.js
+++ day02/index.js
@@ -17,7 +17,7 @@ const validPasswordsOne = inputItems.filter(item => {
 	// then assign them to the variables 'range' and 'letter'
 	const [range, letter] = policy.split(' ');
 
-	// split the range and assign each to variables
+	// split the range convert them to numbers and assign each to variables
 	const [lowEndRange, highEndRange] = range.split('-').map(Number);
 
 	// initialize a variable to hold the count of occurances of the letter in the password
@@ -36,3 +36,35 @@ const validPasswordsOne = inputItems.filter(item => {
 });
 // log the length of the new array
 console.log('Part one answer: ', validPasswordsOne.length); // 410
+
+///////////////////////////////////////////////
+
+const validPasswordsTwo = inputItems.filter(item => {
+	// split an array item into two parts along the :,
+	// assigning the first part to the variable 'policy' and the second to the variable 'password' using DESTRUCTURING ASSIGNMENT
+	//then trim the password
+	let [policy, password] = item.split(':');
+	password = password.trim();
+
+	// split the policy into two parts along the space between the two positions and the letter to be checked,
+	// then assign them to the variables 'positionPair' and 'letter'
+	const [positionPair, letter] = policy.split(' ');
+
+	// split the positions, convert them to Number type, and assign each to variables
+	const [positionOne, positionTwo] = positionPair.split('-').map(Number);
+
+	// assign the letters at position one and position two in the password (non-zero indexed) to variables
+	const letterAtPosOne = password[positionOne - 1];
+	const letterAtPosTwo = password[positionTwo - 1];
+
+	// check if password is valid based on if letter is at positionOne and not at positionTwo
+	if (letterAtPosOne !== letter && letterAtPosTwo !== letter) {
+		return false;
+	} else if (letterAtPosOne === letter && letterAtPosTwo === letter) {
+		return false;
+	} else {
+		return true;
+	}
+});
+// log the length of the new array
+console.log('Part two answer: ', validPasswordsTwo.length); //
